1f647296d5391fd12d94abc0ecf9ab89ee8e6e0b
pushpraj33 <pushprajkumar640@gmail.com>
2026-02-23 10:49:52 +0530

    Add files via upload

---
+++ nthfibnumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fib1(long n)
+{
+    if(n==0) return 0;
+    if(n==1) return 1;
+   return fib1(n-1)+fib1(n-2);
+}
+int fib2(long n)
+{
+    long arr[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+}
+int main()
+{
+    //printf("%1d ",fib1(8));
+    printf("%1d ",fib2(8));
+}
